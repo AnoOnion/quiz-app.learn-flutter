43c85a5f0f04c0a061d60d39ef070bd1e4b04f56
AnoOnion <anoonion25@gmail.com>
2023-10-27 10:25:54 +0700

    added data and models for question

---
+++ lib/data/question_data.dart
@@ -0,0 +1,94 @@
+import 'package:quiz_app/models/question_model.dart';
+
+const questions = [
+  QuestionModel(
+    'What programming language is primarily used for Flutter development ?',
+    [
+      'JavaScript',
+      'Python',
+      'Dart',
+      'Ruby',
+    ],
+  ),
+  QuestionModel(
+    'Which IDE (Integrated Development Environment) is commonly used for Flutter app development ?',
+    [
+      'Visual Studio Code',
+      'Adobe Photoshop',
+      'Sublime Text',
+      'Microsoft Word',
+    ],
+  ),
+  QuestionModel(
+    'What widget in Flutter is used to create a button with user interaction ?',
+    [
+      'TextBlock',
+      'BoxModel',
+      'InkWell',
+      'ImageContainer',
+    ],
+  ),
+  QuestionModel(
+    'What does the Flutter term "Hot Reload" refer to ?',
+    [
+      'A popular Flutter game',
+      'A way to quickly restart your computer',
+      'A feature for making instant code changes and seeing them in the app',
+      'A tool for detecting bugs in your code',
+    ],
+  ),
+  QuestionModel(
+    'What is the purpose of the "pubspec.yaml" file in a Flutter project ?',
+    [
+      'To store user preferences',
+      'To list project dependencies and configure settings',
+      'To create a backup of the code',
+      "To define the app's primary color",
+    ],
+  ),
+  QuestionModel(
+    'Which Flutter class is used for creating a scrollable list of widgets ?',
+    [
+      'Stack',
+      'Grid',
+      'ListView',
+      'Column',
+    ],
+  ),
+  QuestionModel(
+    'What is the purpose of the "setState" method in Flutter ?',
+    [
+      'To delete a widget from the screen',
+      'To fetch data from the internet',
+      "To rebuild the widget's UI and update the screen",
+      'To play an animation',
+    ],
+  ),
+  QuestionModel(
+    'What is the core building block for creating user interfaces in Flutter ?',
+    [
+      'Blocks',
+      'Buttons',
+      'Widgets',
+      'Panels',
+    ],
+  ),
+  QuestionModel(
+    "What is the Flutter framework's primary target platform for app development ?",
+    [
+      'Windows',
+      'macOS',
+      'Android and iOS',
+      'Linux',
+    ],
+  ),
+  QuestionModel(
+    'In Flutter, what is the purpose of the "Navigator" class ?',
+    [
+      'To control the state of your app',
+      'To handle network requests',
+      'To manage app navigation and route transitions',
+      'To draw graphics on the screen',
+    ],
+  ),
+];
